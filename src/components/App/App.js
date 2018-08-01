5fdaf6f367dfabdc30993870ee3546f46ed5cf83
Fernando Guevara <luisfernandoguevara95@gmail.com>
2018-08-01 17:51:24 -0500

    Validate form

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,13 +1,38 @@
 import React, { Component } from 'react';
 
 import Header from '../Header/Header'
+import Form from '../Form/Form'
 
 class App extends Component {
+
+  state = {
+    error: ''
+  }
+
+  componentDidMount() {
+    this.setState({
+      error: false
+    })
+  }
+
+  dataQuery = (response) => {
+    if(response.country === '' || response.city === '') {
+      this.setState({
+        error: true
+      })
+    } else {
+      console.log('todo fine');
+    }
+  }
+
   render() {
     return (
       <div className="app">
         <Header
           title='Clima App'
+        />
+        <Form
+        dataQuery = {this.dataQuery}
         />
       </div>
     );
---
+++ src/components/Form/Form.js
@@ -0,0 +1,58 @@
+import React, { Component } from 'react';
+
+class Form extends Component {
+
+    // Crear los refs
+    countryRef = React.createRef();
+    cityRef = React.createRef();
+
+
+    searchWeather = (e) => {
+        e.preventDefault();
+        // Leer los refs y crear el objeto
+        const response = {
+            country: this.countryRef.current.value,
+            city : this.cityRef.current.value
+        }
+
+        // Enviar por props
+        this.props.dataQuery(response);
+
+        // Opcional resetear el form
+    }
+
+    render() { 
+        return <div className="form-container">
+            <div className="container">
+              <div className="row">
+                    <form onSubmit={this.searchWeather}>
+                        <div className="input-field col s12 m8 l4 offset-m2">
+                            <select ref={this.countryRef}>
+                                <option value="" defaultValue >
+                                    Elige un País
+                                </option>
+                                <option value="AR">Argentina</option>
+                                <option value="CO">Colombia</option>
+                                <option value="CR">Costa Rica</option>
+                                <option value="ES">España</option>
+                                <option value="US">Estados Unidos</option>
+                                <option value="MX">México</option>
+                                <option value="PE">Perú</option>
+                            </select>
+                            <label htmlFor="country">País</label>
+                        </div>
+                        <div className="input-field col s12 m8 l4 offset-m2">
+                            <input ref={this.cityRef} id="city" type="text" />
+                            <label htmlFor="city">Ciudad:</label>
+                        </div>
+                        <div className="input-field col s12 m8 l4 offset-m2 search">
+                            <input type="submit" className="waves-effect waves-light btn-large yellow accent-4" value="Buscar..."/>
+                        </div>
+                    </form>
+              </div>
+            </div>
+          </div>;
+    }
+}
+ 
+export default Form;
