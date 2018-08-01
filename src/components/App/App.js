fd0aaf31d16a9cb16d5c47e7e833eab322a97e9b
Fernando Guevara <luisfernandoguevara95@gmail.com>
2018-08-01 18:09:24 -0500

    Add error component

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -2,38 +2,50 @@ import React, { Component } from 'react';
 
 import Header from '../Header/Header'
 import Form from '../Form/Form'
+import Error from '../Error/Error';
 
 class App extends Component {
 
-  state = {
-    error: ''
-  }
+    state = {
+        error: ''
+    }
 
   componentDidMount() {
     this.setState({
-      error: false
+        error: false
     })
   }
 
   dataQuery = (response) => {
     if(response.country === '' || response.city === '') {
-      this.setState({
-        error: true
-      })
+        this.setState({
+            error: true
+        })
     } else {
-      console.log('todo fine');
+        console.log('todo fine');
     }
   }
 
   render() {
+
+    const error = this.state.error;
+    let result;
+
+    if(error) {
+        result = <Error message = 'Ambos campos son obligatorios' />
+    }
+
+    console.log(error);
+
     return (
       <div className="app">
         <Header
-          title='Clima App'
+            title='Clima App'
         />
         <Form
         dataQuery = {this.dataQuery}
         />
+        {result}
       </div>
     );
   }
